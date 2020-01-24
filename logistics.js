9336d9f7fe3ea7e78ec24665dfeef8ba7eabc8bc
dnthakur <41033523+dnthakur@users.noreply.github.com>
2020-01-25 01:14:53 +0530

    Update logistics.js

--- logistics.js
+++ logistics.js
@@ -28,7 +28,7 @@ var target_div = document.getElementById('preview')
 // });
 //-----------------------------------------------------------------
 function onSignIn(googleUser) {
-    $('.container').style.display="block";
+    $('.container')[0].style.display="block";
     console.log("user Signed in");
   var profile = googleUser.getBasicProfile();
     console.log($('#docId'));
