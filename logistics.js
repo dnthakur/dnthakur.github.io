ef09f147c91824d3174dcdf9f68bf01ce2d78c9e
dnthakur <41033523+dnthakur@users.noreply.github.com>
2020-01-24 19:33:04 +0530

    Update logistics.js

--- logistics.js
+++ logistics.js
@@ -29,10 +29,11 @@ var target_div = document.getElementById('preview')
 //-----------------------------------------------------------------
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  $(#'docId').innerHTML=profile.getId()); // Do not send to your backend! Use an ID token instead.
+  $('#docName').innerHTML=profile.getName();
+    $("#docImg").src=getImageUrl();
+  //$(#docImg).style.=getImageUrl());
+  //console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
 
